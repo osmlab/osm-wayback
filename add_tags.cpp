a347516b079ae869bf271ee9c12d53a580499065
Lukas Martinelli <me@lukasmartinelli.ch>
2017-07-05 15:35:50 -0400

    Allow specify index dir

--- add_tags.cpp
+++ add_tags.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
+#include <unistd.h>
 #include <sstream>
 
 #pragma GCC diagnostic push
@@ -16,10 +17,21 @@
 #include "rocksdb/db.h"
 
 int main(int argc, char* argv[]) {
+    std::string index_filename;
+    const int remaining_args = argc - optind;
+    if (remaining_args == 1) {
+        index_filename = argv[optind];
+        std::cerr << "Using OSM history from index'" << index_filename << "'..." << std::endl;
+    } else {
+        std::cerr << "Usage: " << argv[0] << " [OPTIONS] INDEX_DIR" << std::endl;
+        std::exit(1);
+    }
+
     rocksdb::DB* db;
     rocksdb::Options options;
     options.create_if_missing = true;
-    rocksdb::Status status = rocksdb::DB::Open(options, "/tmp/testdb", &db);
+    rocksdb::Status status = rocksdb::DB::Open(options, index_filename, &db);
+
     rapidjson::Document doc;
     for (std::string line; std::getline(std::cin, line);) {
         if(doc.Parse<0>(line.c_str()).HasParseError()) {
